4cbd11fd54d81f8d483f356ad43d031cee7d5f4e
Mohamed Nidal <touhami9@gmail.com>
2023-08-13 17:16:19 +0100

    loading from a fake api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
+import { useEffect, useReducer } from 'react';
 import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: payload,
+        status: 'ready',
+      };
+
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+
+    default:
+      throw new Error('Invalid action type: ' + type);
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchData() {
+      try {
+        const res = await fetch('http://localhost:8000/questions', {
+          signal: controller.signal,
+        });
+
+        const data = await res.json();
+
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+      </Main>
     </div>
   );
 }
