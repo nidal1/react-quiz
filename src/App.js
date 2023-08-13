505fea93a4e96db9d21211d6cede1eb9c0ac31fc
Mohamed Nidal <touhami9@gmail.com>
2023-08-13 20:21:06 +0100

    displaying the progress

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Error from './components/Error';
 import Start from './components/Start';
 import Question from './components/Question';
 import NextButton from './components/NextButton';
+import Progress from './components/Progress';
 
 const initialState = {
   questions: [],
@@ -62,12 +63,16 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
   useEffect(() => {
     const controller = new AbortController();
     async function fetchData() {
@@ -103,6 +108,13 @@ function App() {
         )}
         {status === 'active' && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
